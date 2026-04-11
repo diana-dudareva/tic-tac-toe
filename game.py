ecfa76501597e0adc169b50bc2d3533e4be015f6
diana-dudareva <dudareva_0283>
2026-04-11 19:18:47 +0300

    exception handling

--- game.py
+++ game.py
@@ -1,10 +1,21 @@
 from gameparts import Board
 
+from gameparts.exceptions import FieldIndexError
+
 
 def main():
     game = Board()
     game.display()
-    game.make_move(1, 1, 'X')
+
+    row = int(input('Введите номер строки: '))
+    if row < 0 or row >= game.field_size:
+        raise FieldIndexError
+    
+    column = int(input('Введите номер столбца: '))
+    if column < 0 or column >= game.field_size:
+        raise FieldIndexError
+    
+    game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
 
