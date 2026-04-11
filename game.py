6a63ea19e07f752f1513a226a3c742aa1506e3f8
diana-dudareva <dudareva_0283>
2026-04-11 21:10:08 +0300

    recording results

--- game.py
+++ game.py
@@ -2,6 +2,11 @@ from gameparts import Board
 from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 
+def save_result(result):
+    file = open('results.txt', 'a', encoding='utf-8')
+    file.write(result + '\n')
+    file.close()
+
 def main():
     game = Board()
     current_player = 'X'
@@ -9,6 +14,7 @@ def main():
     game.display()
 
     while running:
+
         print(f'Ход делают {current_player}')
 
         while True:
@@ -46,12 +52,18 @@ def main():
         game.make_move(row, column, current_player)
         game.display()
         if game.check_win(current_player):
-            print(f'Победили {current_player}!')
+            result = f'Победили {current_player}.'
+            print(result)
+
+            save_result(result)
             running = False
         elif game.is_board_full():
-            print('Ничья!')
+            result = 'Ничья!'
+            print(result)
+            save_result(result)
             running = False
 
+            
 
         current_player = 'O' if current_player == 'X' else 'X'
 
