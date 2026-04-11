c42a7972f0828072829fa59026fae8e2357a4ac5
diana-dudareva <dudareva_0283>
2026-04-11 21:22:24 +0300

    creating a graphical window

---
+++ pygame_test.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Пример графического окна Pygame')
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
